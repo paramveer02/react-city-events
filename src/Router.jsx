e4f017c9491d3a68d7f730712c046b61c6a98c95
Paramveer <paramveermarwahafc@gmail.com>
2025-08-23 12:35:31 +0200

    feat(events): Create Event via action; typeahead address suggestions; no lat/lon fields

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import EventsPage from "./pages/EventsPage";
-import EventDetail from "./components/events/EventDetail";
-import CreateEventEntry from "./components/events/CreateEventEntry";
+import EventDetail, {
+  eventDetailLoader,
+} from "./components/events/EventDetail";
+import CreateEventEntry, {
+  createEventAction,
+} from "./components/events/CreateEventEntry";
 import ErrorPage from "./pages/ErrorPage";
 import RootLayout from "./layout/RootLayout";
-import SignIn from "./pages/authentication/SignIn";
-import SignUp from "./pages/authentication/SignUp";
+import SignIn, { signinAction } from "./pages/authentication/SignIn";
+import SignUp, { signupAction } from "./pages/authentication/SignUp";
 import AuthContextProvider from "./contexts/AuthContext";
+import MyEvents from "./pages/MyEvents";
+import AIGuidePage from "./pages/AiGuidePage";
 
 const router = createBrowserRouter([
   {
@@ -17,10 +23,31 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <HomePage /> },
       { path: "events", element: <EventsPage /> },
-      { path: "events/:id", element: <EventDetail /> },
-      { path: "create", element: <CreateEventEntry /> },
-      { path: "signup", element: <SignUp /> },
-      { path: "signin", element: <SignIn /> },
+      { path: "myevents", element: <MyEvents /> },
+      {
+        path: "events/:id",
+        element: <EventDetail />,
+        loader: eventDetailLoader,
+      },
+      {
+        path: "create",
+        element: <CreateEventEntry />,
+        action: createEventAction,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+        action: signupAction,
+      },
+      {
+        path: "/signin",
+        element: <SignIn />,
+        action: signinAction,
+      },
+      {
+        path: "/ai-guide",
+        element:<AIGuidePage />
+      },
     ],
   },
 ]);
